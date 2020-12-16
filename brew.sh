4f29cdd7c91e2789425924287313b8c4ef4f89b3
Karol Molnár <molnarkarol@gmail.com>
2020-12-16 13:48:06 +0100

    change: brew.sh update

--- brew.sh
+++ brew.sh
@@ -27,50 +27,37 @@ brew install fzf
 brew install rg
 brew install gibo
 brew install cheat
-brew install cmus
-brew install aria2
+#brew install cmus
+#brew install aria2
 
 # Install zsh and make it default
 brew install zsh
 sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 
 echo "Installing apps..."
-brew tap caskroom/cask
 
-APPS=(
-    alfred
-    android-studio
-    appcleaner
-    basictex
-    contexts
-    dropbox
-    enpass
-    flux
-    iterm2
-    jettison
-    karabiner-elements
-    limechat
-    macvim
-    marked
-    paragon-ntfs
-    parallels
-    scroll-reverser
-    skype
-    teamviewer
-    the-unarchiver
-    vlc
-    xnviewmp
-)
-
-brew cask install ${APPS[@]}
+brew install --cask alfred
+brew install --cask android-studio
+brew install --cask appcleaner
+brew install --cask basictex
+brew install --cask contexts
+brew install --cask dropbox
+brew install --cask enpass
+brew install --cask iterm2
+brew install --cask jettison
+brew install --cask karabiner-elements
+brew install --cask macvim
+brew install --cask marked
+brew install --cask paragon-ntfs
+brew install --cask parallels
+brew install --cask scroll-reverser
+brew install --cask skype
+brew install --cask the-unarchiver
+brew install --cask vlc
+brew install --cask xnviewmp
 
 echo "Installing fonts..."
-brew tap caskroom/fonts
-FONTS=(
-    font-dejavu-sans-mono-for-powerline
-)
-
-brew cask install ${FONTS[@]}
+brew install --cask font-dejavu-sans-mono-for-powerline
 
 brew tap nickolasburr/pfa
 brew install sc-im
